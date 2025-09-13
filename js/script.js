c3dd9cd41841523cebd5b16f99f0a8fd9a10a4ab
JM <143664295+sharkleadership@users.noreply.github.com>
2025-09-13 15:03:30 -0500

    Add content and jQuery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// JM, 09/09/25
+// JM, 09/13/25
 
 $(document).ready(function() {
-    
+    // WIP: Featured project carousel
+
+    // const slides = $('.project__carousel li');
+
+    // const firstEl = slides.get(0);
+    // const firstClone = $(firstEl).clone();
+
+    // const lastEl = slides.get(-1);
+    // const lastClone = $(lastEl).clone();
+    // $('.project__carousel').append(firstClone);
+    // $('.project__carousel').prepend(lastClone);
+
+    // END
+
+
+    $(".other-projects .project__card:even").addClass('even');
+
+    $(".other-projects .project__card").hover(function() {
+        const description = $(this).children('figcaption').html();
+        $("#other-projects__description").html(description);
+
+        $("#other-projects__description").slideDown('fast');
+    },
+    function() {
+        $("#other-projects__description").slideUp('fast');
+    });
 });
