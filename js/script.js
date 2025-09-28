8ce67103878a2afff88bc07ca3cdf96386e6a7a9
JM <143664295+sharkleadership@users.noreply.github.com>
2025-09-28 18:11:19 -0500

    Adjust styles of carousel

--- js/script.js
+++ js/script.js
@@ -3,30 +3,68 @@
 $(document).ready(function() {
     // WIP: Featured project carousel
 
-    // const slides = $('.project__carousel li');
+    // const carousel = $('.project__carousel');
+    // const slides = $('.project__carousel .slide');
 
+    // // Create clones at the beginning and end to simulate infinite scroll
     // const firstEl = slides.get(0);
     // const firstClone = $(firstEl).clone();
 
     // const lastEl = slides.get(-1);
     // const lastClone = $(lastEl).clone();
-    // $('.project__carousel').append(firstClone);
-    // $('.project__carousel').prepend(lastClone);
+    // carousel.append(firstClone);
+    // carousel.prepend(lastClone);
+
+    // let currSlide = 1;
+    // let interval;
+    // const time = 3000;
+
+    // const startCarousel = () => {
+    //     interval = setInterval(() => {
+    //         nextSlide();
+    //         console.log(currSlide);
+    //     }, time);
+    // };
+
+    // const nextSlide = () => {
+    //     const slides = $('.project__carousel .slide');
+    //     if (currSlide >= slides.length - 1) return;
+        
+    //     currSlide++;
+
+    //     const width = slides.first().width();
+    //     carousel.animate({transform: `translateX(${-width * currSlide}px)`}, "swing");
+    // };
+
+    // slides.bind("transitionend", () => {
+    //     // if ()
+    // });
+
+    // // Pause on hover
+    // carousel.hover(
+    //     () => {
+    //         clearInterval(interval);
+    //     },
+    //     () => startCarousel()
+    // );
+
+    // startCarousel();
 
     // END
 
-
     $(".other-projects .project__card:even").addClass('even');
 
-    $(".other-projects .project__card").hover(function() {
-        $(this).addClass("gradient-blue");
-        const description = $(this).children('figcaption').html();
-        $("#other-projects__description").html(description);
+    $(".other-projects .project__card").hover(
+        function() {
+            $(this).addClass("gradient-blue");
+            const description = $(this).children('figcaption').html();
+            $("#other-projects__description").html(description);
 
-        $("#other-projects__description").slideDown('fast');
-    },
-    function() {
-        $(this).removeClass("gradient-blue");
-        $("#other-projects__description").slideUp('fast');
-    });
+            $("#other-projects__description").slideDown('fast');
+        },
+        function() {
+            $(this).removeClass("gradient-blue");
+            $("#other-projects__description").slideUp('fast');
+        }
+    );
 });
