f2bdf495e777834080e7cd90167ff301315ae214
JM <143664295+sharkleadership@users.noreply.github.com>
2025-12-04 17:37:53 -0600

    Add user-initiated animation - colophon open/close

---
+++ final/js/script.js
@@ -0,0 +1,15 @@
+// JM, 12/04/25
+// Final Project, WEB 238
+
+// Colophon - User-initiated Animation
+$('.colophon__open').click(function() {
+    $('.colophon').toggle('slow', 'swing', function() {
+        $(this).attr('open', !$(this).attr('open'));
+    });
+});
+
+$('.colophon__close').click(function() {
+    $('.colophon').hide('slow', 'swing', function() {
+        $(this).attr('open', false);
+    });
+});
