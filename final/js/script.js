d4f96c98f5417c7d7d09734a662dbdc9017fe41d
JM <143664295+sharkleadership@users.noreply.github.com>
2025-12-04 20:36:54 -0600

    Add translation of credits

--- final/js/script.js
+++ final/js/script.js
@@ -1,15 +1,21 @@
 // JM, 12/04/25
 // Final Project, WEB 238
 
+// Translate Colophon - User Interaction
+
+$("button.translate").click(function() {
+    $(".untranslated, .translated").toggle();
+});
+
 // Colophon - User-initiated Animation
-$('.colophon__open').click(function() {
-    $('.colophon').toggle('slow', 'swing', function() {
-        $(this).attr('open', !$(this).attr('open'));
+$(".colophon__open").click(function() {
+    $(".colophon").toggle("slow", "swing", function() {
+        $(this).attr("open", !$(this).attr("open"));
     });
 });
 
-$('.colophon__close').click(function() {
-    $('.colophon').hide('slow', 'swing', function() {
-        $(this).attr('open', false);
+$(".colophon__close").click(function() {
+    $(".colophon").hide("slow", "swing", function() {
+        $(this).attr("open", false);
     });
 });
